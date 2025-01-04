a91a32ddace35531c54dc719a1eb1c784dbc2ac6
emodulTerbaru <emodulfives@gmail.com>
2025-01-04 12:50:18 +0700

    Update projek1.py

--- projek1.py
+++ projek1.py
@@ -1,3 +1,43 @@
 import streamlit as st
 
-st.write("Hello world")
+import streamlit as st
+from pyrebase import pyrebase
+
+# Konfigurasi Firebase
+firebaseConfig = {
+    "apiKey": "AIzaSyC9vDQ3wyq1Z4rqre7tVdvZRmUt8TGomo4",
+    "authDomain": "percobaan-pertama-ae4ff.firebaseapp.com",
+    "databaseURL": "https://percobaan-pertama-ae4ff-default-rtdb.firebaseio.com",
+    "projectId": "percobaan-pertama-ae4ff",
+    "storageBucket": "percobaan-pertama-ae4ff.firebasestorage.app",
+    "messagingSenderId": "298037230291",
+    "appId": "1:298037230291:web:73839320cd74cc9dba3226",
+    "measurementId": "G-BSVCLVBYX6"
+}
+
+# Inisialisasi Pyrebase
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+# Menambahkan data ke Realtime Database
+def add_data_realtime(name, age):
+    db.child("users").push({"name": name, "age": age})
+
+# Mendapatkan data dari Realtime Database
+def get_data_realtime():
+    users = db.child("users").get()
+    return users.val()
+
+# Streamlit UI
+st.title("Firebase Realtime Database with Streamlit")
+
+name = st.text_input("Nama:")
+age = st.number_input("Umur:", min_value=0, step=1)
+
+if st.button("Tambah Data"):
+    add_data_realtime(name, age)
+    st.success("Data berhasil ditambahkan!")
+
+if st.button("Lihat Data"):
+    data = get_data_realtime()
+    st.table(data)
